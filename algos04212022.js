57273144f97c593464cd57f6fcdf3f3bbc903c32
Waseem <wahid.waseem@gmail.com>
2022-05-12 12:44:14 -0700

    new algos

---
+++ algos04212022.js
@@ -0,0 +1,52 @@
+function countBy(x, n) {
+    let z = [];
+    for (i = 1; i <= n; i++) {
+        z.push(i * x)
+    }
+    return z;
+}
+
+
+console.log(countBy(1, 10))
+console.log(countBy(2, 5))
+
+// make a rock paper scissors game 
+const rps = (p1, p2) => {
+    if (p1 === p2) {
+        return "Draw!"
+    }
+    const rules = {
+        rock: 'scissors',
+        paper: 'rock',
+        scissors: 'paper'
+    };
+    if (rules[p1] === p2) {
+        return `Player 1 won!`;
+    } else {
+        return `Player 2 won!`;
+    }
+};
+
+console.log(rps('scissors', 'paper'))
+console.log(rps('scissors', 'rock'))
+console.log(rps('paper', 'paper'))
+
+// double the input array
+function maps(x) {
+    return x.map(n => n * 2)
+}
+console.log(maps([1, 2, 3, 4]))
+
+
+function strCount(str, letter) {
+
+    let count = 0;
+
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] == letter)
+            count++;
+    }
+
+    return count;
+
+}
